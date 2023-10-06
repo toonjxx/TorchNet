f5e6d507a2fcea896201e4f7abfda71de083aeea
MasterT <thanhtrungjt@gmail.com>
2023-10-06 16:17:22 +0700

    first commit

---
+++ main.py
@@ -0,0 +1,147 @@
+import os
+from pprint import pformat
+from typing import Any
+
+import ignite.distributed as idist
+from data import setup_data
+from ignite.engine import Events
+from ignite.handlers import PiecewiseLinear
+from ignite.metrics import Accuracy, Loss
+from ignite.utils import manual_seed
+from models import TransformerModel
+from torch import nn, optim
+from trainers import setup_evaluator, setup_trainer
+from utils import *
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"  # remove tokenizer paralleism warning
+
+
+def run(local_rank: int, config: Any):
+    # make a certain seed
+    rank = idist.get_rank()
+    manual_seed(config.seed + rank)
+
+    # create output folder and copy config file to output dir
+    output_dir = setup_output_dir(config, rank)
+    if rank == 0:
+        save_config(config, output_dir)
+
+    config.output_dir = output_dir
+
+    # donwload datasets and create dataloaders
+    dataloader_train, dataloader_eval = setup_data(config)
+
+    config.num_iters_per_epoch = len(dataloader_train)
+
+    # model, optimizer, loss function, device
+    device = idist.device()
+    model = idist.auto_model(
+        TransformerModel(
+            config.model,
+            config.model_dir,
+            config.drop_out,
+            config.n_fc,
+            config.num_classes,
+        )
+    )
+
+    config.lr *= idist.get_world_size()
+    optimizer = idist.auto_optim(optim.AdamW(model.parameters(), lr=config.lr, weight_decay=config.weight_decay))
+    loss_fn = nn.BCEWithLogitsLoss().to(device=device)
+
+    le = config.num_iters_per_epoch
+    milestones_values = [
+        (0, 0.0),
+        (le * config.num_warmup_epochs, config.lr),
+        (le * config.max_epochs, 0.0),
+    ]
+    lr_scheduler = PiecewiseLinear(optimizer, param_name="lr", milestones_values=milestones_values)
+
+    # setup metrics to attach to evaluator
+    metrics = {
+        "Accuracy": Accuracy(output_transform=thresholded_output_transform),
+        "Loss": Loss(loss_fn),
+    }
+
+    # trainer and evaluator
+    trainer = setup_trainer(config, model, optimizer, loss_fn, device, dataloader_train.sampler)
+    evaluator = setup_evaluator(config, model, metrics, device)
+
+    # setup engines logger with python logging
+    # print training configurations
+    logger = setup_logging(config)
+    logger.info("Configuration: \n%s", pformat(config))
+    trainer.logger = evaluator.logger = logger
+
+    trainer.add_event_handler(Events.ITERATION_COMPLETED, lr_scheduler)
+
+    # setup ignite handlers
+    to_save_train = {
+        "model": model,
+        "optimizer": optimizer,
+        "trainer": trainer,
+        "lr_scheduler": lr_scheduler,
+    }
+    to_save_eval = {"model": model}
+    ckpt_handler_train, ckpt_handler_eval = setup_handlers(trainer, evaluator, config, to_save_train, to_save_eval)
+
+    # experiment tracking
+    if rank == 0:
+        exp_logger = setup_exp_logging(config, trainer, optimizer, evaluator)
+
+    # print metrics to the stderr
+    # with `add_event_handler` API
+    # for training stats
+    trainer.add_event_handler(
+        Events.ITERATION_COMPLETED(every=config.log_every_iters),
+        log_metrics,
+        tag="train",
+    )
+
+    # run evaluation at every training epoch end
+    # with shortcut `on` decorator API and
+    # print metrics to the stderr
+    # again with `add_event_handler` API
+    # for evaluation stats
+    @trainer.on(Events.EPOCH_COMPLETED(every=1))
+    def _():
+        evaluator.run(dataloader_eval, epoch_length=config.eval_epoch_length)
+        log_metrics(evaluator, "eval")
+
+    # let's try run evaluation first as a sanity check
+    @trainer.on(Events.STARTED)
+    def _():
+        evaluator.run(dataloader_eval, epoch_length=config.eval_epoch_length)
+
+    # setup if done. let's run the training
+    trainer.run(
+        dataloader_train,
+        max_epochs=config.max_epochs,
+        epoch_length=config.train_epoch_length,
+    )
+
+    # close logger
+    if rank == 0:
+        exp_logger.close()
+
+    # show last checkpoint names
+    logger.info(
+        "Last training checkpoint name - %s",
+        ckpt_handler_train.last_checkpoint,
+    )
+
+    logger.info(
+        "Last evaluation checkpoint name - %s",
+        ckpt_handler_eval.last_checkpoint,
+    )
+
+
+# main entrypoint
+def main():
+    config = setup_config()
+    with idist.Parallel(config.backend) as p:
+        p.run(run, config=config)
+
+
+if __name__ == "__main__":
+    main()
---
+++ trainers.py
@@ -0,0 +1,80 @@
+from typing import Any, Dict, Union
+
+import ignite.distributed as idist
+import torch
+from ignite.engine import DeterministicEngine, Engine, Events
+from ignite.metrics.metric import Metric
+from torch import nn
+from torch.cuda.amp import autocast, GradScaler
+from torch.optim.optimizer import Optimizer
+from torch.utils.data import DistributedSampler, Sampler
+
+
+def setup_trainer(
+    config: Any,
+    model: nn.Module,
+    optimizer: Optimizer,
+    loss_fn: nn.Module,
+    device: Union[str, torch.device],
+    train_sampler: Sampler,
+) -> Union[Engine, DeterministicEngine]:
+    scaler = GradScaler(enabled=config.use_amp)
+
+    def train_function(engine: Union[Engine, DeterministicEngine], batch: Any):
+        input_ids = batch["input_ids"].to(device, non_blocking=True, dtype=torch.long)
+        attention_mask = batch["attention_mask"].to(device, non_blocking=True, dtype=torch.long)
+        token_type_ids = batch["token_type_ids"].to(device, non_blocking=True, dtype=torch.long)
+        labels = batch["label"].view(-1, 1).to(device, non_blocking=True, dtype=torch.float)
+
+        model.train()
+
+        with autocast(enabled=config.use_amp):
+            y_pred = model(input_ids, attention_mask, token_type_ids)
+            loss = loss_fn(y_pred, labels)
+
+        optimizer.zero_grad()
+        scaler.scale(loss).backward()
+        scaler.step(optimizer)
+        scaler.update()
+
+        metric = {"train_loss": loss.item()}
+        engine.state.metrics = metric
+        return metric
+
+    trainer = Engine(train_function)
+
+    # set epoch for distributed sampler
+    @trainer.on(Events.EPOCH_STARTED)
+    def set_epoch():
+        if idist.get_world_size() > 1 and isinstance(train_sampler, DistributedSampler):
+            train_sampler.set_epoch(trainer.state.epoch - 1)
+
+    return trainer
+
+
+def setup_evaluator(
+    config: Any,
+    model: nn.Module,
+    metrics: Dict[str, Metric],
+    device: Union[str, torch.device],
+):
+    @torch.no_grad()
+    def evalutate_function(engine: Engine, batch: Any):
+        model.eval()
+
+        input_ids = batch["input_ids"].to(device, non_blocking=True, dtype=torch.long)
+        attention_mask = batch["attention_mask"].to(device, non_blocking=True, dtype=torch.long)
+        token_type_ids = batch["token_type_ids"].to(device, non_blocking=True, dtype=torch.long)
+        labels = batch["label"].view(-1, 1).to(device, non_blocking=True, dtype=torch.float)
+
+        with autocast(enabled=config.use_amp):
+            output = model(input_ids, attention_mask, token_type_ids)
+
+        return output, labels
+
+    evaluator = Engine(evalutate_function)
+
+    for name, metric in metrics.items():
+        metric.attach(evaluator, name)
+
+    return evaluator
