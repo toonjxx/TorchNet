7fd65ece1e9560a4ac5daf52b33edb5aee153168
MasterT <thanhtrungjt@gmail.com>
2023-10-11 13:29:02 +0700

    modifi dataset & dataloader

--- src/custom_dataset.py
+++ src/custom_dataset.py
@@ -2,49 +2,52 @@ import os
 import pandas as pd
 import torch
 from torch.utils.data import DataLoader, Dataset
-from utils import load_config
-
 
 # Define a custom PyTorch dataset class for your time series data
 class CustomDataset(Dataset):
-    def __init__(self, csv_files, labels_file):
-        self.csv_files = csv_files
+    def __init__(self, data_dir, labels_file,label_select,transform=None):
+        """
+        Arguments:
+            labels_file (string): Path to the csv file with labels.
+            data_dir (string): Directory with all the sample.
+            transform (callable, optional): Optional transform to be applied
+            on a sample.
+        """
+        self.data_dir = data_dir
         self.labels_df = pd.read_csv(labels_file)
-
-        # Load settings from the config file
-        config = load_config()
-        self.data_dir = config["data_dir"]
+        self.label_select = label_select
 
     def __len__(self):
-        return len(self.csv_files)
+        return len(self.labels_df)
 
     def __getitem__(self, idx):
-        csv_file = self.csv_files[idx]
-        df = self.load_sample_csv(csv_file)
+        sample_id = self.labels_df["IDSample"][idx]
+        label_select = self.label_select
+        data = self.load_data(os.path.join(self.data_dir,f"{sample_id}.csv"))
 
-        # Extract features (time series data) and labels
-        features = df.iloc[:, :].values  # Assuming the last 4 columns are labels
-
-        # Extract labels based on the sample file's name (assuming a common format)
-        sample_id = os.path.basename(csv_file).split(".")[0]
-        labels_row = self.labels_df[self.labels_df["ID"] == sample_id]
-
+        labels_row = self.labels_df.iloc[idx,:]
         if not labels_row.empty:
-            labels = labels_row.iloc[:, 1:].values
+            if label_select == "BP":
+                SBP = labels_row["SBP"]
+                DBP = labels_row["DBP"]
+                labels = pd.array([SBP,DBP])
+
+            elif label_select == "Hypertension":
+                labels = labels_row["Hypertension"]
+            else:
+                labels = None
         else:
             labels = None  # Handle cases where labels are not found
 
         # Convert to PyTorch tensors
-        features = torch.FloatTensor(features)
+        data = torch.FloatTensor(data.values)
         if labels is not None:
             labels = torch.FloatTensor(labels)
+        return data, labels
 
-        return features, labels
-
-    def load_sample_csv(self, sample_csv_file):
+    def load_data(self, sample_csv_file):
         try:
-            df = pd.read_csv(sample_csv_file)
-
+            df = pd.read_csv(sample_csv_file,header=None)
             return df
         except Exception as e:
             print(f"Error loading CSV file {sample_csv_file}: {str(e)}")
--- src/data_loader.py
+++ src/data_loader.py
@@ -8,11 +8,8 @@ def create_data_loader(split, batch_size):
     config = load_config()
     data_dir = config["data_dir"]
     data_dir = os.path.abspath(data_dir)
-    data_folder = os.path.join(data_dir,split)
-    csv_files = [os.path.join(data_folder, file) for file in data_folder if file.endswith(".csv")]
-
-    labels_file = Path(os.path.join(data_folder,'Label.csv'))
-
-    dataset = CustomDataset(csv_files, labels_file)
+    data_dir = os.path.join(data_dir,split)
+    labels_file = Path(os.path.join(data_dir,'Label.csv'))
+    dataset = CustomDataset(data_dir, labels_file,label_select="BP")
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
     return dataloader
